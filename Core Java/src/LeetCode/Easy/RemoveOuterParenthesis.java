0a12cf61f7a2b59d80d2a75e394c0376833b341c
kdesai <kdesai@KDESAI.ad.nts.net>
2019-05-02 11:28:12 -0700

    modified

--- Core Java/src/LeetCode/Easy/RemoveOuterParenthesis.java
+++ Core Java/src/LeetCode/Easy/RemoveOuterParenthesis.java
@@ -1,5 +1,32 @@
 package LeetCode.Easy;
+/*
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
 
+Example 1:
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+Example 2:
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+
+Example 3:
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ */
 
 public class RemoveOuterParenthesis
 {
@@ -11,12 +38,14 @@ public class RemoveOuterParenthesis
         {
             if (S.charAt(i) == '(')
             {
-                if (open++ == 0)
+                if (open == 0)
                 	start = i;
+                open++;
             }
             else
             {
-                if (--open == 0)
+            	open--;
+                if (open == 0)
                 	sb.append(S.substring(start+1, i));
             }
         }
