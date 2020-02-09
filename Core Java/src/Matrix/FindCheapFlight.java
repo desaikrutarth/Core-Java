fbd3619926fd831de18c81c7d88946437fbc9fdd
kdesai <kdesai@KDESAI-W10.ad.nts.net>
2020-02-08 22:26:22 -0800

    Updated Dijkstra's Algorithm

--- Core Java/src/Matrix/FindCheapFlight.java
+++ Core Java/src/Matrix/FindCheapFlight.java
@@ -1,6 +1,7 @@
 package Matrix;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 give a list of cities a to city b the price of air tickets for example 
@@ -12,7 +13,13 @@ Now let you from city x to city y, you cannot transfer more than twice between t
 find the cheap flight from x to y, follow up print out the flight
  */
 /**
-	
+		    200
+  		 1------2
+ 	 100/ \100 /
+	   /   \  /50
+	  0------3
+        400
+        	
 0->1 100
 1->2 200
 2->3 50
@@ -24,11 +31,74 @@ find the cheap flight from x to y, follow up print out the flight
 1 100  0   200  100 
 2 0   200  0    50 
 3 400 100  50   0 
+
 */
 public class FindCheapFlight 
 {
+	static int dijkstra(int[][] adjacentMatrix)
+	{
+		int numOfVertex = adjacentMatrix.length;
+		int[] distance = new int[numOfVertex];	
+		boolean visited[] = new boolean[numOfVertex];
+		
+		Arrays.fill(distance, Integer.MAX_VALUE);		//Fill all distance to Infinity
+		distance[0] = 0;								//Start with first node
+		
+		for(int i=0; i < numOfVertex-1; i++)
+		{
+			//Find Vertex with minimum distance
+			int minVertex = findMinVertex(distance, visited);
+			visited[minVertex] = true;
+			
+			//Explore neighbours
+			for(int j=0; j < numOfVertex; j++)
+			{
+				if(adjacentMatrix[minVertex][j] != 0 && !visited[j] && distance[minVertex] != Integer.MAX_VALUE)
+				{
+					int newDistance = distance[minVertex] + adjacentMatrix[minVertex][j];
+					if(newDistance < distance[j])
+					{
+						distance[j] = newDistance;
+					}
+				}
+			}
+		}
+		
+		// Printing minimum distance from City A to each Cities
+		for(int i=1; i<numOfVertex; i++)
+		{
+			System.out.println("Minimum distance of Vertex 0 to Vertex "+i+" = "+distance[i]);
+		}
+		System.out.println();
+		
+		return distance[numOfVertex-1];	
+	}
+	
+	static int findMinVertex(int[] distance, boolean visited[])
+	{
+		int minVertex = -1;
+		for(int i=0; i<distance.length; i++)
+		{
+			if(!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex]))
+			{
+				minVertex = i;
+			}
+		}
+		return minVertex;
+	}
+	
+	/*public static int mincost(int[][] nodes)
+	{
+		List<Integer> visited = new ArrayList<Integer>();
+		int org = 0;
+	    int dest = 3;
+	    visited.add(org);
+	    
+	    return mincostHelper(nodes, org, dest, 0, 0, visited);
+	}
+	
 	static int mincost =  Integer.MAX_VALUE;
-	public static int mincost(int[][] nodes, int org, int dest, int hops, int cost, List<Integer> visited)
+	public static int mincostHelper(int[][] nodes, int org, int dest, int hops, int cost, List<Integer> visited)
 	{
 	    if(org == dest)
 	    {
@@ -49,25 +119,21 @@ public class FindCheapFlight
 	      if(connections[i] != 0 && !visited.contains(i))
 	      {
 	        visited.add(i);
-	      	mincost = mincost(nodes, i, dest, hops+1, cost+connections[i], visited);
+	      	mincost = mincostHelper(nodes, i, dest, hops+1, cost+connections[i], visited);
 	        visited.remove(new Integer(i));
 	      }
 	    }
 	    return mincost;
 	  }
-	  
+	  */
 	  public static void main(String[] args)
 	  {  
 		    int[][] nodes = new int[][]{{0, 100, 0, 400},
 		    							{100, 0, 200, 100},
 		    							{0, 200, 0, 50},
 		    							{400, 100, 50, 0}};
-		    int org = 0;
-		    int dest = 3;
-		    
-		    List<Integer> visited = new ArrayList<Integer>();
-		    visited.add(org);
-		    int n = mincost(nodes, org, dest, 0, 0, visited);
-		  	System.out.println(n);
-		  }
+		    							
+		   System.out.println("Minimum distance from City A to City D = "+dijkstra(nodes));
+		  //System.out.println(mincost(nodes));
+	  }
 }
--- Data Structure/src/Graph/DijkstraShortestPath.java
+++ Data Structure/src/Graph/DijkstraShortestPath.java
@@ -1,26 +1,50 @@
 package Graph;
 
 import java.util.Arrays;
-
+/*
+  Below is a graph for given matrix where vertex are 0 to 4 and their distance between 2 vertex are given.
+  Find the minimum distance to reach from vertex 0 to 4.
+  
+  			5
+  		 1------3
+ 	  4 /|     /|
+	   / |   5/ |
+	  0  |2  /  |4
+  	   \ |  /   |
+  	  8 \| /    |
+  		 2------4
+  			9
+  			
+Distance from 0 to 1 -> 4
+Distance from 0 to 2 -> 8
+Distance from 1 to 2 -> 2
+Distance from 1 to 3 -> 5
+Distance from 2 to 3 -> 5
+Distance from 2 to 4 -> 9
+Distance from 3 to 4 -> 4 
+ */
 public class DijkstraShortestPath 
 {
+	static int numOfVertex = 0;
+	static int[] distance;
+	
 	static void dijkstra(int[][] adjacentMatrix)
 	{
-		int v = adjacentMatrix.length;
-		boolean visited[] = new boolean[v];
-		int[] distance = new int[v];	
+		numOfVertex = adjacentMatrix.length;
+		distance = new int[numOfVertex];	
+		boolean visited[] = new boolean[numOfVertex];
 		
 		Arrays.fill(distance, Integer.MAX_VALUE);		//Fill all distance to Infinity
 		distance[0] = 0;								//Start with first node
 		
-		for(int i=0; i < v-1; i++)
+		for(int i=0; i < numOfVertex-1; i++)
 		{
 			//Find Vertex with minimum distance
 			int minVertex = findMinVertex(distance, visited);
 			visited[minVertex] = true;
 			
 			//Explore neighbours
-			for(int j=0; j < v; j++)
+			for(int j=0; j < numOfVertex; j++)
 			{
 				if(adjacentMatrix[minVertex][j] != 0 && !visited[j] && distance[minVertex] != Integer.MAX_VALUE)
 				{
@@ -32,9 +56,14 @@ public class DijkstraShortestPath
 				}
 			}		
 		}
-		
-		for(int i=0; i<v; i++)
-			System.out.println(i + " "+ distance[i]);
+	}
+	
+	public static void printDistance()
+	{
+		for(int i=1; i<numOfVertex; i++)
+		{
+			System.out.println("Minimum distance of Vertex 0 to Vertex "+i+" = "+distance[i]);
+		}
 	}
 	
 	static int findMinVertex(int[] distance, boolean visited[])
@@ -60,5 +89,28 @@ public class DijkstraShortestPath
 							 {0,0,0,0,0}
 										};
 		dijkstra(adjMatrix);
+		printDistance();
 	}
 }
+
+/*
+  Below is a graph for given matrix where vertex are 0 to 4 and their distance between 2 vertex are given.
+  
+  			5
+  		 1------3
+ 	  4 /|     /|
+	   / |   5/ |
+	  0  |2  /  |4
+  	   \ |  /   |
+  	  8 \| /    |
+  		 2------4
+  			9
+  			
+  	 0 to 1 -> 4
+  	 0 to 2 -> 8
+  	 1 to 2 -> 2
+  	 1 to 3 -> 5
+  	 2 to 3 -> 5
+  	 2 to 4 -> 9
+  	 3 to 4 -> 4 
+ */
