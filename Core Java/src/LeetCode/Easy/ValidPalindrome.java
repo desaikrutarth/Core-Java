6a010e02537ea0f1668669bd532e30904ef852e1
kruta <kruta@LAPTOP-HMHQ0FI1>
2020-09-28 16:32:50 -0500

    Updated Valid Palindrome Leetcode easy

--- Core Java/src/LeetCode/Easy/ValidPalindrome.java
+++ Core Java/src/LeetCode/Easy/ValidPalindrome.java
@@ -13,7 +13,33 @@ Explanation: You could delete the character 'c'.
  */
 public class ValidPalindrome
 {
-	public static boolean validPalindrome(String str)
+	//O(n)
+	public static boolean validPalindrome(String s)
+	{
+		int l = -1, r = s.length();
+	    while(++l <= --r)
+	    {
+	         if (s.charAt(l) != s.charAt(r)) 
+	         {
+	             return isPalindrome(s, l + 1, r)
+	                 || isPalindrome(s, l, r - 1);
+	         }
+	    }
+	    return true;
+	}
+	    
+	static boolean isPalindrome(String s, int from, int to)
+	{
+		while(from++ <= to--) 
+	    {
+	        if (s.charAt(from) != s.charAt(to)) 
+	           	return false;
+	    }
+	    return true;
+	}
+	
+	//O(n)^2
+/*	public static boolean validPalindrome(String str)
     {
 		if(isPalindrome(str))
 			return true;
@@ -32,7 +58,7 @@ public class ValidPalindrome
 	{
 		return new StringBuilder(str).reverse().toString().equals(str);
 	}
-    
+*/
 	public static void main(String[] args) 
 	{
 		System.out.println(validPalindrome("abbca"));
